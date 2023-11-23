90954f373e2c350eac73e788a7335e9c17e92b66
jCarlos <jc.zr88@gmail.com>
2023-11-23 10:59:28 +0100

    primeros pasos

--- src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
@@ -1,9 +1,12 @@
 package org.iesalandalus.programacion.reservashotel;
 
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class MainApp {
     
     public static void main(String[] args) {
-        System.out.println("Hasta luego!!!!");
+
+
     }
 }
